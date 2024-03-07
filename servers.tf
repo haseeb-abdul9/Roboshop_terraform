d009a6bf39999beb3efc2bc974b44ab4b62d6039
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-03-06 23:21:44 -0500

    minor corrections

--- servers.tf
+++ servers.tf
@@ -9,49 +9,51 @@ data "aws_security_group" "allow_all" {
 }
 
 variable "components" {
-  frontend = {
-    name = "frontend"
-    instance_type = "t3.micro"
-  }
-  mongodb = {
-    name = "mongodb"
-    instance_type = "t3.micro"
-  }
-  Catalogue = {
-    name = "catalogue"
-    instance_type = "t3.micro"
-  }
-  redis = {
-    name = "redis"
-    instance_type = "t3.micro"
-  }
-  user = {
-    name = "user"
-    instance_type = "t3.micro"
-  }
-  cart = {
-    name = "cart"
-    instance_type = "t3.micro"
-  }
-  mysql = {
-    name = "mysql"
-    instance_type = "t3.micro"
-  }
-  shipping = {
-    name = "shipping"
-    instance_type = "t3.micro"
-  }
-  rabbitmq = {
-    name = "rabbitmq"
-    instance_type = "t3.micro"
-  }
-  payment = {
-    name = "payment"
-    instance_type = "t3.micro"
-  }
-  dispatch = {
-    name = "dispatch"
-    instance_type = "t3.micro"
+  default = {
+    frontend ={
+      name = "frontend"
+      instance_type = "t3.micro"
+    }
+    mongodb ={
+      name = "mongodb"
+      instance_type = "t3.micro"
+    }
+    Catalogue ={
+      name = "catalogue"
+      instance_type = "t3.micro"
+    }
+    redis ={
+      name = "redis"
+      instance_type = "t3.micro"
+    }
+    user ={
+      name = "user"
+      instance_type = "t3.micro"
+    }
+    cart ={
+      name = "cart"
+      instance_type = "t3.micro"
+    }
+    mysql ={
+      name = "mysql"
+      instance_type = "t3.micro"
+    }
+    shipping ={
+      name = "shipping"
+      instance_type = "t3.micro"
+    }
+    rabbitmq ={
+      name = "rabbitmq"
+      instance_type = "t3.micro"
+    }
+    payment ={
+      name = "payment"
+      instance_type = "t3.micro"
+    }
+    dispatch ={
+      name = "dispatch"
+      instance_type = "t3.micro"
+    }
   }
 }
 
