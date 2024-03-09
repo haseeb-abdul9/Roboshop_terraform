74ee7483c3e9eae3b38ff402f57e28a58abc3309
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-03-09 14:54:36 -0500

    state env

--- env-prod/state.tf
+++ env-prod/state.tf
@@ -1,3 +1,6 @@
 bucket = "terraform-ah-m"
 key    = "roboshop/prod/terrform.tfstate"
 region = "us-east-1"
+bucket = "terraform-ah-m"
+key    = "roboshop/prod/terrform.tfstate"
+region = "us-east-1"
