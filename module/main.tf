338511cb9e144902c95f361eef8cf19feae565fa
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-04-20 21:27:20 -0400

    learning ansible loops

--- module/main.tf
+++ module/main.tf
@@ -53,6 +53,35 @@ resource "aws_iam_role" "role" {
   }
 }
 
+resource "aws_iam_role_policy" "ssm_ps_policy" {
+  name = "${var.component_name}-${var.env}-policy"
+  role = aws_iam_role.role
+
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "arn:aws:ssm:us-east-1:018632729566:parameter/dev.frontend"
+      },
+      {
+        "Sid": "VisualEditor1",
+        "Effect": "Allow",
+        "Action": "ssm:DescribeParameters",
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
 
 
 
