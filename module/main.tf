94c54e100b7922244c0bb5b19fcf898ef46de031
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-04-21 05:13:03 -0400

    learning ansible loops

--- module/main.tf
+++ module/main.tf
@@ -82,7 +82,7 @@ resource "aws_iam_role_policy" "ssm_ps_policy" {
       {
         "Sid": "VisualEditor1",
         "Effect": "Allow",
-        "Action": "kms:Decrypt",
+        "Action": "ssm:Decrypt",
         "Resource": "*"
       },
     ]
