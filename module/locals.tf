404eb8a5a881082d76aaf63400318b7c05b5aa2b
haseeb-abdul9 <129119790+haseeb-abdul9@users.noreply.github.com>
2024-04-22 11:17:47 -0400

    Update locals.tf

--- module/locals.tf
+++ module/locals.tf
@@ -8,6 +8,6 @@ locals {
   ]
   app_commands = [
     "sudo labauto ansible",
-  #     "ansible-pull -i localhost, -U https://github.com/haseeb-abdul9/roboshop_ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+    "ansible-pull -i localhost, -U https://github.com/haseeb-abdul9/roboshop_ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
 }
