a157b06678d0386f9c76e13f3872341c12ddf26d
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-04-23 12:47:10 -0400

    learning ansible loops

--- module/main.tf
+++ module/main.tf
@@ -59,35 +59,6 @@ resource "aws_iam_instance_profile" "instance_profile" {
   role = aws_iam_role.role.name
 }
 
-# resource "aws_iam_role_policy" "ssm_ps_policy" {
-#   name = "${var.component_name}-${var.env}-ssm-ps-policy"
-#   role = aws_iam_role.role.id
-#
-#   policy = jsonencode({
-#     "Version": "2012-10-17",
-#     "Statement": [
-#       {
-#         "Sid": "VisualEditor0",
-#         "Effect": "Allow",
-#         "Action": [
-#           "ssm:GetParameterHistory",
-#           "ssm:GetParametersByPath",
-#           "ssm:GetParameters",
-#           "ssm:GetParameter",
-#           "kms:GetParametersForImport",
-#
-#         ],
-#         "Resource": "arn:aws:ssm:us-east-1:018632729566:parameter/${var.env}.${var.component_name}.*"
-#       },
-#       {
-#         "Sid": "VisualEditor1",
-#         "Effect": "Allow",
-#         "Action": "ssm:Decrypt",
-#         "Resource": "*"
-#       },
-#     ]
-#   })
-# }
 resource "aws_iam_role_policy" "ssm_ps_policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
   role = aws_iam_role.role.id
