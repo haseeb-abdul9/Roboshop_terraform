d1f162712844b0667299e901e91e61554e5bbef8
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-07-14 17:48:17 -0400

    prometheus node exporter

--- module/locals.tf
+++ module/locals.tf
@@ -10,4 +10,11 @@ locals {
     "sudo labauto ansible",
     "ansible-pull -i localhost, -U https://github.com/haseeb-abdul9/roboshop_ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
+  db_tags = {
+    Name = "${var.component_name}-${var.env}"
+  }
+  app_tags = {
+    Name = "${var.component_name}-${var.env}"
+    Monitor = "true"
+  }
 }
